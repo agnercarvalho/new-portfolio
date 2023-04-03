3aca864f1b4000374a75b8e26c75c3a32cd5ff92
Agner C <agnerf@gmail.com>
2023-04-03 20:57:57 -0300

    Inicialização do projeto

---
+++ data.js
@@ -0,0 +1,11 @@
+const data = [
+    {nome: 'Projeto 1',
+    categoria: 'Identidade Visual',
+    desc: 'BlaBla'},
+    {nome: 'Projeto 2',
+    categoria: 'Identidade Visual',
+    desc: 'BlaBla'},
+    {nome: 'Projeto 3',
+    categoria: 'Identidade Visual',
+    desc: 'BlaBla'}
+]
