631bb91c47d1fb876928ef64c86d960f97502c5e
Agner C <agnerf@gmail.com>
2023-04-07 00:39:18 -0300

    Fist look at JS logic

---
+++ scripts.js
@@ -0,0 +1,9 @@
+const categoryContainer = document.querySelectorAll(".category-container");
+
+console.log(categoryContainer)
+
+categoryContainer.forEach(category => {
+    category.parentElement.addEventListener('click', (e) => {
+        
+    })
+});
